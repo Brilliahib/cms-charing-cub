d5fbf9f2c0871b5561890a79c96479604ae15b37
brilliahib <brilliahib21@gmail.com>
2024-12-23 09:39:15 +0700

    feat: get all booking from daycare

---
+++ src/http/daycares/bookings/get-all-booking-from-daycare.ts
@@ -0,0 +1,37 @@
+import { UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+import { api } from "@/lib/axios";
+import { BookingDaycare } from "@/types/booking/booking";
+
+interface GetAllBookingFromDaycaresResponse {
+  data: BookingDaycare[];
+}
+
+export const getAllBookingFromDaycaresHandler = async (
+  token: string
+): Promise<GetAllBookingFromDaycaresResponse> => {
+  const { data } = await api.get<GetAllBookingFromDaycaresResponse>(
+    "/daycares/booking/list",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return data;
+};
+
+export const useGetAllBookingFromDaycares = (
+  token: string,
+  options?: Partial<
+    UseQueryOptions<GetAllBookingFromDaycaresResponse, AxiosError>
+  >
+) => {
+  return useQuery({
+    queryKey: ["booking-from-daycares"],
+    queryFn: () => getAllBookingFromDaycaresHandler(token),
+    ...options,
+  });
+};
