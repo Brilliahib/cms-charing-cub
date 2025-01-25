26d89bd8c9fe3a135a180c6c5b4718a952eed79c
brilliahib <brilliahib21@gmail.com>
2025-01-25 20:29:15 +0700

    feat: transform for article and create nearby daycare

--- src/components/organism/home/HomeContent.tsx
+++ src/components/organism/home/HomeContent.tsx
@@ -1,11 +1,14 @@
 import HomeArticle from "./HomeArticle";
 import HomeHero from "./HomeHero";
 import HomeIntroVideo from "./HomeIntroVideo";
+import HomeNearbyDaycare from "./HomeNearbyDaycare";
 
 export default function HomeContent() {
   return (
     <>
       <HomeHero />
+      {/* If there are already many daycares */}
+      {/* <HomeNearbyDaycare /> */}
       <HomeArticle />
       <HomeIntroVideo />
     </>
--- src/components/organism/home/HomeNearbyDaycare.tsx
+++ src/components/organism/home/HomeNearbyDaycare.tsx
@@ -1,22 +1,20 @@
 "use client";
 
-import { useGetArticle } from "@/http/article/get-all-article";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetAllDaycare } from "@/http/daycares/get-all-daycares";
 import { baseUrl } from "@/utils/app";
-import { format } from "date-fns";
-import { id } from "date-fns/locale";
 import Image from "next/image";
 import Link from "next/link";
-import { Skeleton } from "@/components/ui/skeleton";
 
-export default function HomeArticle() {
-  const { data, isPending } = useGetArticle();
+export default function HomeNearbyDaycare() {
+  const { data, isPending } = useGetAllDaycare();
 
   return (
     <>
       <div className="pad-x md:pt-24 pt-16 space-y-12">
         <div className="md:w-[70rem] w-fit">
           <h1 className="font-bold tracking-tighter text-3xl sm:text-5xl text-zinc-700 relative z-10">
-            Our Article🤝
+            Nearby Daycare🤝
           </h1>
           <span className="md:w-[20rem] w-[10rem] h-[2rem] bg-gradient-to-r from-purple-500 to-purple-100 absolute -rotate-2 -translate-y-7 z-0 opacity-30"></span>
         </div>
@@ -30,27 +28,25 @@ export default function HomeArticle() {
                   <Skeleton className="h-[16px] w-1/2" />
                 </div>
               ))
-            : data?.data.map((article) => (
+            : data?.data.map((daycare) => (
                 <Link
-                  href={`/articles/${article.id}`}
-                  key={article.id}
+                  href={`/daycares/${daycare.id}`}
+                  key={daycare.id}
                   className="space-y-2 hover:-translate-y-1 transition-transform duration-300 ease-in-out"
                 >
                   <Image
-                    src={`${baseUrl}/${article.image}`}
-                    alt={article.title}
+                    src={`${baseUrl}/${daycare.facility_images[0].image_url}`}
+                    alt={daycare.name}
                     width={1000}
                     height={1000}
                     className="h-[180px] md:w-full w-full object-cover rounded-xl"
                   />
                   <div className="space-y-1">
                     <h1 className="font-semibold md:text-md text-base line-clamp-2">
-                      {article.title}
+                      {daycare.name}
                     </h1>
-                    <p className="text-sm text-muted-foreground">
-                      {format(new Date(article.created_at), "d MMMM yyyy", {
-                        locale: id,
-                      })}
+                    <p className="text-sm text-muted-foreground line-clamp-2">
+                      {daycare.location}
                     </p>
                   </div>
                 </Link>
