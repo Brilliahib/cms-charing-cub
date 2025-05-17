babf12834b192ae71dbbea5e4c7c2970f8177ff1
brilliahib <brilliahib21@gmail.com>
2025-05-17 22:19:11 +0700

    feat: select type on booking daycares to paid and all

--- src/components/organism/dashboard/daycares/DaycareBookingFromDaycareContent.tsx
+++ src/components/organism/dashboard/daycares/DaycareBookingFromDaycareContent.tsx
@@ -4,10 +4,18 @@ import { bookingDaycareFromDaycareColumns } from "@/components/atoms/datacolumn/
 import SearchQueryInput from "@/components/atoms/search/SearchQueryInput";
 import { DataTable } from "@/components/molecules/datatable/DataTable";
 import PaginationComponent from "@/components/molecules/pagination/Pagination";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useGetAllBookingDaycareList } from "@/http/daycares/bookings/get-all-booking-daycare-list";
+import { useGetAllBokingDaycareIsPaid } from "@/http/daycares/bookings/get-all-booking-daycare-paid";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function DaycareBookingFromDaycareContent() {
   const { data: session, status } = useSession();
@@ -18,6 +26,7 @@ export default function DaycareBookingFromDaycareContent() {
   const [currentPage, setCurrentPage] = useState(
     Number(searchParams.get("page")) || 1
   );
+  const [filterStatus, setFilterStatus] = useState("all");
 
   const { data, isPending } = useGetAllBookingDaycareList(
     session?.access_token as string,
@@ -26,12 +35,27 @@ export default function DaycareBookingFromDaycareContent() {
     { enabled: status === "authenticated" }
   );
 
+  const { data: bookingPaid } = useGetAllBokingDaycareIsPaid(
+    session?.access_token as string,
+    query,
+    currentPage,
+    {
+      enabled: status === "authenticated" && filterStatus === "paid",
+    }
+  );
+
+  useEffect(() => {
+    router.push(`?query=${query}&page=${currentPage}&status=${filterStatus}`);
+  }, [filterStatus]);
+
   const [searchQuery, setSearchQuery] = useState("");
 
-  const filteredData =
-    data?.data.filter((daycares) =>
-      daycares.name_babies.toLowerCase().includes(searchQuery.toLowerCase())
-    ) || [];
+  const selectedData =
+    filterStatus === "paid" ? bookingPaid?.data || [] : data?.data || [];
+
+  const filteredData = selectedData.filter((daycare) =>
+    daycare.name_babies.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
@@ -43,25 +67,51 @@ export default function DaycareBookingFromDaycareContent() {
     setCurrentPage(page);
     router.push(`?query=${query}&page=${page}`);
   };
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterStatus(e.target.value);
+  };
+
   return (
     <>
       <div className="py-4 space-y-8">
-        <div className="flex w-full">
+        <div className="flex flex-col md:justify-between md:flex-row md:items-center gap-4 w-full">
           <SearchQueryInput
             value={query}
             onChange={handleSearch}
             placeholder="Cari berdasarkan nama anak"
             className="md:w-[350px]"
           />
+          <Select value={filterStatus} onValueChange={setFilterStatus}>
+            <SelectTrigger className="md:w-[200px] w-full">
+              <SelectValue placeholder="Filter pembayaran" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">Semua</SelectItem>
+              <SelectItem value="paid">Sudah Dibayar</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
         <DataTable
           columns={bookingDaycareFromDaycareColumns()}
           data={filteredData}
         />
         <PaginationComponent
-          totalItems={data?.pagination.total || 0}
-          itemsPerPage={data?.pagination.per_page || 10}
-          currentPage={data?.pagination.current_page || 1}
+          totalItems={
+            filterStatus === "paid"
+              ? bookingPaid?.pagination.total || 0
+              : data?.pagination.total || 0
+          }
+          itemsPerPage={
+            filterStatus === "paid"
+              ? bookingPaid?.pagination.per_page || 10
+              : data?.pagination.per_page || 10
+          }
+          currentPage={
+            filterStatus === "paid"
+              ? bookingPaid?.pagination.current_page || 1
+              : data?.pagination.current_page || 1
+          }
           onPageChange={handlePageChange}
         />
       </div>
